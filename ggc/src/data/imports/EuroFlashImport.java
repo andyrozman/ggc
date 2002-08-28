238567717fa51c4b8cce35b45c1c97f31fe22318
Dieter Schultschik <schultd@users.sourceforge.net>
2002-08-28 13:07:28 +0000

    new dataimport classes

---
+++ ggc/src/data/imports/EuroFlashImport.java
@@ -0,0 +1,264 @@
+/*
+ * Created on 10.08.2002
+ *
+ * To change this generated comment edit the template variable "filecomment":
+ * Window>Preferences>Java>Templates.
+ */
+package data.imports;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import javax.comm.NoSuchPortException;
+import javax.comm.SerialPortEvent;
+import javax.swing.ImageIcon;
+
+import datamodels.DailyValuesRow;
+import event.*;
+
+/**
+ * @author stephan
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class EuroFlashImport extends SerialMeterImport {
+
+	private int counter = 0;
+
+	private DailyValuesRow[] importedData = new DailyValuesRow[0];
+
+	public static final int CONTROL_FIRST = 0x40;
+
+	public static final int CONTROL_SECOND = 0x3F;
+
+	/**
+	 * Constructor for EuroFlashImport.
+	 */
+	public EuroFlashImport() {
+		super();
+		
+		setImage(new ImageIcon(getClass().getResource("/icons/euroflash.png")));
+		setUseInfoMessage(
+			"<html>"+
+			"Please connect your cable with your meter.<br>"+
+			"Turn your meter off and press \"Import\""+
+			"</html>"
+		);
+		setName("EuroFlash");
+	}
+
+	/**
+	 * @see data.imports.DataImport#importData()
+	 */
+	public void importData() throws ImportException {
+		super.importData();
+
+		try {
+			portOutputStream.write((int) 'D');
+			portOutputStream.write((int) 'M');
+			portOutputStream.write((int) '@');
+		} catch (IOException e) {
+		}
+	}
+
+	/**
+	 * @see data.imports.DataImport#getImportedData()
+	 */
+	public DailyValuesRow[] getImportedData() {
+		return importedData;
+	}
+	
+
+	/**
+	 * @see javax.comm.SerialPortEventListener#serialEvent(SerialPortEvent)
+	 */
+	public void serialEvent(SerialPortEvent event) {
+		super.serialEvent(event);
+		
+		if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+
+			try {
+				StringBuffer inputBuffer = createBufferFromStream(portInputStream);
+
+				char ident = inputBuffer.charAt(0);
+
+				if (ident == '@') {
+					portOutputStream.write((int) 'D');
+					portOutputStream.write((int) 'M');
+					portOutputStream.write((int) '?');
+					timeOut += 15000;
+					System.out.println("second char written.");
+				}
+
+				if (ident == '?') {
+					portOutputStream.write((int) 'D');
+					portOutputStream.write((int) 'M');
+					portOutputStream.write((int) 'P');
+					timeOut += 30000;
+					System.out.println("third char written.");
+				}
+
+				if (ident == 'P') {
+					timeOut += 15000;
+					parseDataString(new String(inputBuffer));
+					stopImport();
+					
+				}
+
+			} catch (Exception exc) {
+				System.out.println("Exception while parsing new data. " + exc);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private StringBuffer createBufferFromStream(InputStream stream) {
+		StringBuffer inputBuffer = new StringBuffer();
+		int newData = 0;
+
+		while (newData != -1) {
+			try {
+				newData = stream.read();
+				if (newData == -1) {
+					break;
+				}
+				if ('\r' == (char) newData) {
+					inputBuffer.append('\n');
+				} else {
+					//System.out.println("byte : " + newData);
+					inputBuffer.append((char) newData);
+				}
+			} catch (IOException ex) {
+				System.err.println(ex);
+				return inputBuffer;
+			}
+		}
+
+		return inputBuffer;
+	}
+
+	private void parseDataString(String dataStr) throws ImportException {
+		String value = null;
+		Vector importDataVector = new Vector();
+
+		try {
+			int currentCount = 0;
+			int valueCount = 0;
+			String measure = "";
+
+			StringTokenizer stk = new StringTokenizer(dataStr, "\n");
+
+			String tmp = null;
+			StringTokenizer vstk = null;
+
+			////////////////////////////////////////
+			value = stk.nextToken();
+			vstk = new StringTokenizer(value, ",");
+
+			// first value is information
+			// ex.: P 150,"NNS210DPN","MG/DL " 05BD
+			//String subValue = vstk.nextToken();
+
+			// P 150
+			String subValue = vstk.nextToken();
+			valueCount = Integer.decode(subValue.substring(2)).intValue();
+
+			// "NNS210DPN"; not used
+			vstk.nextToken();
+
+			// "MG/DL " 05BD
+			vstk.nextToken();
+			subValue = subValue.substring(1);
+			int pos = subValue.indexOf('"');
+			if (pos != -1) {
+				//Log.getLogger().info("subValue : " + subValue);
+				measure = subValue.substring(0, pos);
+			}
+			////////////////////////////////////////
+
+			// value string: P "SAT"," 6/02/01"," 1:21:30   ","  228 ", 00 07F6
+			//DateFormat dFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, new Locale("en", "US"))
+			SimpleDateFormat dFormat = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
+			while (stk.hasMoreTokens()) {
+				value = stk.nextToken();
+
+				vstk = new StringTokenizer(value, ",");
+
+				// P "SAT"; I don't need it
+				if (!vstk.hasMoreTokens())
+					continue;
+				subValue = vstk.nextToken();
+
+				// " 6/02/01"," 1:21:30   " ; the day mm/dd/yy
+				if (!vstk.hasMoreTokens())
+					continue;
+				subValue = vstk.nextToken();
+				String dayValue = subValue.substring(1, subValue.length() - 2).trim();
+
+				if (!vstk.hasMoreTokens())
+					continue;
+				subValue = vstk.nextToken();
+				String timeValue = subValue.substring(1, subValue.length() - 2).trim();
+
+				Date date = dFormat.parse(dayValue + " " + timeValue, new ParsePosition(0));
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(date);
+				cal.roll(Calendar.YEAR, 2000);
+				date = cal.getTime();
+
+				// "  228 " bzValue
+				if (!vstk.hasMoreTokens())
+					continue;
+				subValue = vstk.nextToken();
+				subValue = subValue.substring(1, subValue.length() - 2).trim();
+				float bzValue = Float.parseFloat(subValue);
+
+				//
+				//Value dataValue = new Value(date, bzValue);
+				DailyValuesRow dataValue = new DailyValuesRow(date, bzValue, 0, 0, 0, 0, "");
+				importDataVector.addElement(dataValue);
+				//Log.getLogger().info("new value : " + dataValue);
+				
+				currentCount++;
+				fireImportChanged(new ImportEvent(this, ImportEvent.PROGRESS, (currentCount * 100 / valueCount)));
+			}
+		} catch (Exception exc) {
+			//System.out.println("parsing failed (" + value + ") : " + exc);
+			//exc.printStackTrace();
+			throw new ImportException(exc);
+		}
+
+		importedData = new DailyValuesRow[importDataVector.size()];
+		importDataVector.copyInto(importedData);
+
+		//Log.getLogger().info("parse count="+valueCount+", measure="+measure);
+		//System.out.println("parsing finished; found : " + getImportedData().length);
+	}
+
+	public static void main(String[] args) {
+		EuroFlashImport importer = new EuroFlashImport();
+
+		try {
+
+			importer.setPort("COM2");
+			importer.open();
+			importer.importData();
+
+		} catch (NoSuchPortException e) {
+			System.out.println("exc : " + e);
+		} catch (ImportException e) {
+			System.out.println("exc : " + e);
+		}
+
+		//importer.close();
+	}
+}
---
+++ ggc/src/data/imports/SerialMeterImport.java
@@ -0,0 +1,386 @@
+/*
+ * Created on 10.08.2002
+ *
+ * To change this generated comment edit the template variable "filecomment":
+ * Window>Preferences>Java>Templates.
+ */
+package data.imports;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.StringTokenizer;
+import java.util.TooManyListenersException;
+
+import javax.comm.CommPortIdentifier;
+import javax.comm.NoSuchPortException;
+import javax.comm.PortInUseException;
+import javax.comm.SerialPort;
+import javax.comm.SerialPortEvent;
+import javax.comm.SerialPortEventListener;
+import javax.comm.UnsupportedCommOperationException;
+import javax.swing.ImageIcon;
+import javax.swing.event.EventListenerList;
+
+import datamodels.DailyValuesRow;
+import event.*;
+
+import util.GGCProperties;
+
+/**
+ * @author stephan
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public abstract class SerialMeterImport implements DataImport, SerialPortEventListener, Runnable {
+
+	private boolean isPortOpen = false;
+	
+	private boolean dataFromMeter = false;
+
+	protected SerialPort serialPort = null;
+
+	protected CommPortIdentifier portIdentifier = null;
+
+	protected OutputStream portOutputStream = null;
+
+	protected InputStream portInputStream = null;
+
+	private long startTime = System.currentTimeMillis();
+	
+	protected long timeOut = 50000;
+
+	private EventListenerList listenerList = new EventListenerList();
+	
+	private ImageIcon image = null;
+	
+	private String useInfoMessage = null;
+	
+	private String name = null;
+
+	/**
+	 * Constructor for SerialMeterImport.
+	 */
+	public SerialMeterImport() {
+		super();
+	}
+
+	public void addImportEventListener(ImportEventListener listener) {
+		listenerList.add(ImportEventListener.class, listener);
+	}
+
+	public void removeImportEventListener(ImportEventListener listener) {
+		listenerList.remove(ImportEventListener.class, listener);
+	}
+
+	protected void fireImportChanged(ImportEvent event) {
+		// Guaranteed to return a non-null array
+		Object[] listeners = listenerList.getListenerList();
+		
+		// Process the listeners last to first, notifying
+		// those that are interested in this event
+		for (int i = listeners.length - 2; i >= 0; i -= 2) {
+			if (listeners[i] == ImportEventListener.class) {
+				// Lazily create the event:
+				if (event == null)
+					event = new ImportEvent(this);
+				((ImportEventListener) listeners[i + 1]).importChanged(event);
+			}
+		}
+	}
+	
+	
+	protected void fireImportFinished() {
+		fireImportChanged(new ImportEvent(this, ImportEvent.IMPORT_FINISHED));
+	}
+	
+	
+	protected void fireImportTimeout() {
+		fireImportChanged(new ImportEvent(this, ImportEvent.TIMEOUT));
+	}
+	
+		
+
+	/**
+	 * Return the COM-Port from which will be read.
+	 * @return String 
+	 */
+	public String getPort() {
+		return portIdentifier.getName();
+	}
+
+	/**
+	 * Set the COM-Port from wich will be read.
+	 * @param String port
+	 */
+	public void setPort(String port) throws NoSuchPortException {
+		portIdentifier = CommPortIdentifier.getPortIdentifier(port);
+	}
+
+	/**
+	 * @see data.imports.DataImport#open()
+	 */
+	public boolean open() throws ImportException {
+		if (isPortOpen)
+			return isPortOpen;
+
+		if (portIdentifier == null) {
+			throw new ImportException("No com port specified");
+		}
+
+		try {
+			serialPort = (SerialPort) portIdentifier.open("ggc", (int) timeOut);
+
+			setConnectionParameters();
+
+			portOutputStream = serialPort.getOutputStream();
+			portInputStream = serialPort.getInputStream();
+			serialPort.notifyOnDataAvailable(true);
+			serialPort.notifyOnBreakInterrupt(true);
+
+			try {
+				Thread.sleep(10000);
+			} catch (Exception exc) {
+			}
+
+			isPortOpen = true;
+			System.out.println("open port : " + portIdentifier.getName());
+			serialPort.addEventListener(this);
+
+		} catch (PortInUseException exc) {
+			throw new ImportException(exc);
+
+		} catch (IOException exc) {
+			throw new ImportException(exc);
+
+		} catch (TooManyListenersException exc) {
+			throw new ImportException(exc);
+		}
+
+		try {
+			serialPort.enableReceiveTimeout(30);
+		} catch (UnsupportedCommOperationException e) {
+		}
+
+		if (isPortOpen)
+			fireImportChanged(new ImportEvent(this, ImportEvent.PORT_OPENED, portIdentifier));
+			
+		return isPortOpen;
+	}
+
+	private void setConnectionParameters() {
+
+		if (serialPort == null)
+			return;
+
+		// Save state of parameters before trying a set.
+		int oldBaudRate = serialPort.getBaudRate();
+		int oldDatabits = serialPort.getDataBits();
+		int oldStopbits = serialPort.getStopBits();
+		int oldParity = serialPort.getParity();
+		int oldFlowControl = serialPort.getFlowControlMode();
+
+		// Set connection parameters, if set fails return parameters object
+		// to original state.
+		try {
+			serialPort.setSerialPortParams(9600, // BuadRate
+			SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+		} catch (UnsupportedCommOperationException e) {
+		}
+		// Set flow control.
+		try {
+			//			serialPort.setFlowControlMode(
+			//					parameters.getFlowControlIn() | 
+			//					parameters.getFlowControlOut()
+			//			);
+			serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
+		} catch (UnsupportedCommOperationException e) {
+			//throw new SerialConnectionException("Unsupported flow control");
+		}
+	}
+
+	/**
+	 * @see data.imports.DataImport#close()
+	 */
+	public void close() {
+		if (!isPortOpen)
+			return;
+			
+		serialPort.removeEventListener();
+		serialPort.close();
+		isPortOpen = false;
+		dataFromMeter = false;
+		System.out.println("close port : " + portIdentifier.getName());
+		fireImportChanged(new ImportEvent(this, ImportEvent.PORT_CLOSED, portIdentifier));
+	}
+
+	/**
+	 * @see data.imports.DataImport#importData()
+	 */
+	public void importData() throws ImportException {
+		if (portIdentifier == null || serialPort == null || portOutputStream == null || portInputStream == null)
+			throw new ImportException("COM-Port is not initialized correct.");
+
+		startTime = System.currentTimeMillis();
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	/**
+	 * @see data.imports.DataImport#getImportedData()
+	 */
+	public DailyValuesRow[] getImportedData() {
+		return new DailyValuesRow[0];
+	}
+
+
+
+	/**
+	 * @see javax.comm.SerialPortEventListener#serialEvent(SerialPortEvent)
+	 */
+	public void serialEvent(SerialPortEvent event) {
+		
+		//System.out.println();
+		
+		// Determine type of event.
+		switch (event.getEventType()) {
+			case SerialPortEvent.DATA_AVAILABLE :
+				dataFromMeter = true;
+				timeOut += 5000;
+				break;
+				
+				
+				// If break event append BREAK RECEIVED message.
+			case SerialPortEvent.BI :
+				System.out.println("recievied break");
+				break;				
+			case SerialPortEvent.CD :
+				System.out.println("recievied cd");
+				break;
+			case SerialPortEvent.CTS :
+				System.out.println("recievied cts");
+				break;
+			case SerialPortEvent.DSR :
+				System.out.println("recievied dsr");
+				break;
+			case SerialPortEvent.FE :
+				System.out.println("recievied fe");
+				break;
+			case SerialPortEvent.OE :
+				System.out.println("recievied oe");
+				break;
+			case SerialPortEvent.PE :
+				System.out.println("recievied pe");
+				break;
+			case SerialPortEvent.RI :
+				System.out.println("recievied ri");
+				break;
+		}
+	}
+
+
+	public long getTimeOut() {
+		return timeOut;
+	}
+	
+	
+	public void stopImport() {
+		timeOut = 1;
+	}
+	
+	
+
+	/**
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		while (System.currentTimeMillis() - startTime < getTimeOut()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException exc) {
+			}
+		}
+
+		if (getImportedData().length > 0)
+			fireImportChanged(new ImportEvent(this, ImportEvent.IMPORT_FINISHED));
+		else
+			fireImportChanged(new ImportEvent(this, ImportEvent.TIMEOUT));
+	}
+	
+
+	/**
+	 * Gets the image
+	 * @return Returns a ImageIcon
+	 */
+	public ImageIcon getImage() {
+		return image;
+	}
+	
+	
+	/**
+	 * Sets the image
+	 * @param image The image to set
+	 */
+	public void setImage(ImageIcon image) {
+		this.image = image;
+	}
+	
+
+	/**
+	 * Gets the useInfoMessage
+	 * @return Returns a String
+	 */
+	public String getUseInfoMessage() {
+		return useInfoMessage;
+	}
+	/**
+	 * Sets the useInfoMessage
+	 * @param useInfoMessage The useInfoMessage to set
+	 */
+	public void setUseInfoMessage(String useInfoMessage) {
+		this.useInfoMessage = useInfoMessage;
+	}
+
+	/**
+	 * Gets the name
+	 * @return Returns a String
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * Sets the name
+	 * @param name The name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public static String[] getAvailableMeters() {
+		GGCProperties properties = GGCProperties.getInstance();
+		String allMeter = properties.get("Meter");
+		
+		if (allMeter == null || allMeter.equals(""))
+			return new String[0];
+			
+		StringTokenizer strTk = new StringTokenizer(allMeter, ";");
+		String[] meter = new String[strTk.countTokens()];
+		int counter = 0;
+		while (strTk.hasMoreTokens()) {
+			meter[counter] = strTk.nextToken();
+			counter++;
+		}
+		
+		return meter;
+	}
+	
+	
+	public static String getMeterClassName(String meterName) {
+		GGCProperties properties = GGCProperties.getInstance();
+		System.out.println("Meter." + meterName + ".class");
+		return properties.get("Meter." + meterName + ".class");
+	}
+
+}
