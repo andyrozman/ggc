8f910170e32e8d9c1371438727691dd0653a91ab
Dieter Schultschik <schultd@users.sourceforge.net>
2002-08-28 13:28:49 +0000

    new events

---
+++ ggc/src/event/GlucoValueEventListener.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 15.08.2002
+ *
+ * To change this generated comment edit the template variable "filecomment":
+ * Window>Preferences>Java>Templates.
+ */
+package event;
+
+import java.util.EventListener;
+
+/**
+ * @author stephan
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public interface GlucoValueEventListener extends EventListener {
+	
+	void glucoValuesChanged(GlucoValueEvent event);
+
+}
