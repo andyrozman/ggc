7bcadda22c2bc3dd2ce1a75e8670f6296615f955
Innominate <innominate227@users.sourceforge.net>
2008-08-13 02:09:07 +0000

    company for psudeo meters

---
+++ ggc-meter/src/ggc/meter/manager/company/PseudoMeters.java
@@ -0,0 +1,101 @@
+/*
+ *  GGC - GNU Gluco Control
+ *
+ *  A pure java app to help you manage your diabetes.
+ *
+ *  See AUTHORS for copyright information.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Filename: MeterManager.java
+ *  Purpose:  This class contains all definitions for Meters. This includes:
+ *        meter names, classes that handle meter and all other relevant data.
+ *
+ *  Author:   andyrozman
+ */
+
+
+package ggc.meter.manager.company; 
+
+import ggc.meter.device.pseudo.EZManagerDBMeter;
+import ggc.meter.manager.MeterImplementationStatus;
+
+public class PseudoMeters extends AbstractMeterCompany
+{
+
+    
+    public PseudoMeters()
+    {
+        this.addDevice(new EZManagerDBMeter());
+    }
+
+
+
+    
+    
+    
+    //********************************************************
+    //***      Meter Company Identification Methods        ***
+    //********************************************************
+
+
+    /**
+     * getName - Get Name of meter. 
+     * 
+     * @return name of meter
+     */
+    public String getName()
+    {
+        return "PseudoMeters";
+    }
+
+    
+    /**
+     * getCompanyId - Get Company Id 
+     * 
+     * @return id of company
+     */
+    public int getCompanyId()
+    {
+        return 999;
+    }
+    
+    
+    /**
+     * getInstructions - get instructions for device
+     * 
+     * @return instructions for reading data 
+     */
+    public String getDescription()
+    {
+       return "PseudoMeters"; 
+    }
+    
+    
+    
+    /**
+     * getImplementationStatus - Get Implementation status 
+     * 
+     * @return implementation status as number
+     * @see ggc.meter.manager.MeterImplementationStatus
+     */
+    public int getImplementationStatus()
+    {
+        return MeterImplementationStatus.FUNCTIONALITY_READ_DATA_FULL;
+    }
+    
+    
+    
+}
